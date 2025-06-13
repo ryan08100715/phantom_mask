a9fa66b8af11612306109b8437edb7661abaef8c
Ryan WSY <ryan08100715@gmail.com>
2025-06-13 22:36:31 +0800

    feat: add common exceptions handle

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +17,57 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions
+            ->render(function (NotFoundHttpException $e, Request $request) {
+                if ($request->is('api/*')) {
+                    // 處理模型查無資料
+                    if ($e->getPrevious() instanceof ModelNotFoundException) {
+                        /** @var ModelNotFoundException $modelNotFound */
+                        $modelNotFound = $e->getPrevious();
+
+                        return response()->json([
+                            'message' => $modelNotFound->getMessage(),
+                            'errors' => [
+                                [
+                                    'code' => 'resource_not_found',
+                                    'detail' => $modelNotFound->getMessage(),
+                                ],
+                            ],
+                        ], 404);
+                    }
+                }
+            })
+            ->render(function (ValidationException $e, Request $request) {
+                if ($request->is('api/*')) {
+                    // 處理請求參數驗證錯誤
+                    $errors = [];
+                    foreach ($e->errors() as $error) {
+                        foreach ($error as $errorMessage) {
+                            $errors[] = [
+                                'code' => 'invalid_format',
+                                'detail' => $errorMessage,
+                            ];
+                        }
+                    }
+
+                    return response()->json([
+                        'message' => $e->getMessage(),
+                        'errors' => $errors,
+                    ], 422);
+                }
+            })
+            ->render(function (Exception $e, Request $request) {
+                if ($request->is('api/*')) {
+                    // 處理剩下的錯誤
+                    return response()->json([
+                        'message' => $e->getMessage(),
+                        'errors' => [
+                            [
+                                'code' => 'server_error',
+                                'detail' => $e->getMessage(),
+                            ],
+                        ],
+                    ], 500);
+                }
+            });
     })->create();
